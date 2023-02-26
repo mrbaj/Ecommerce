07372f9f3e0e0cfa8a6013a572f8ad7d693e6099
rahuldhillo <anemailformac@gmail.com>
2023-02-26 11:38:02 +0530

    created login page test case

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,23 @@
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.visit('http://localhost:4200');
+describe('Login page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200'); // Assumes your login page route is '/login'
+  });
+
+  it('should allow user to type in username and password fields', () => {
+    cy.get('input[placeholder="Username"]').type('testuser');
+    cy.get('input[placeholder="Password"]').type('testpass');
+  });
+
+  it('should allow user to sign in', () => {
+    cy.get('input[placeholder="Username"]').type('testuser');
+    cy.get('input[placeholder="Password"]').type('testpass');
+    cy.contains('Sign In').click();
+    cy.url().should('eq', 'http://localhost:4200/home'); // Assumes successful login should redirect to '/home'
+  });
+
+  it('should allow user to sign out', () => {
+    // Assumes user is already logged in
+    cy.get('button[color="warn"]').click();
+    cy.url().should('eq', 'http://localhost:4200/login'); // Assumes successful logout should redirect to '/login'
   });
 });
