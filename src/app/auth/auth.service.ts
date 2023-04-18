b57d7d23927989e1456cf613763be29ce5b420b9
rahuldhillo <anemailformac@gmail.com>
2023-04-18 13:37:37 +0530

    - created server.js and auth.service.ts

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(
+    username: string,
+    password: string
+  ): Observable<{ accessToken: string }> {
+    return this.http.post<{ accessToken: string }>(`${this.apiUrl}/login`, {
+      username,
+      password,
+    });
+  }
 }
